1f17dc2af3ef94f1132aeeaadc0e8b0153e76d95
zZ-O-O-M <stepanyukun@mail.ru>
2021-05-02 23:01:20 +0700

    add some styles

--- src/components/Forms/MicroorganismsCatalog.tsx
+++ src/components/Forms/MicroorganismsCatalog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, useRouteMatch, useParams, Link} from 'react-router-dom';
+import {Link, Route, Switch, useParams, useRouteMatch} from 'react-router-dom';
 import {getAllGenera, getAllStrains, getAllStrainTypes} from '../../utils/data_fetch';
 import Strain, {Genus, StrainType} from '../../models/strain/strain';
 import {useAsync} from 'react-async';
@@ -50,7 +50,9 @@ function GenusList({genusList}: { genusList: Genus[] }) {
                 {genusList.map((genus) => (
                     <li className="genus-list__item genus-item" key={genus.id}>
                         <Link className="genus-list__link genus-link" to={join(url, genus.id.toString())}>
-                            <span className="genus-link__name genus-name">{genus.name}</span>
+                            <span
+                                className="genus-link__name genus-name">{genus.name.length >= 50 ?
+                                genus.name.substr(1, 49) + "..." : genus.name}</span>
                             <span className="genus-link__number genus-strains-number">{listNumber}</span>
                         </Link>
                     </li>
@@ -60,24 +62,25 @@ function GenusList({genusList}: { genusList: Genus[] }) {
     );
 }
 
+
 function TypeList({typeList}: { typeList: StrainType[] }) {
     const {url} = useRouteMatch();
     const {genusIdStr} = useParams<UrlParams>();
 
     return (
-        <nav>
-            <ul>
+        <div className="type-list-block">
+            <ul className="type-list">
                 {typeList
                     .filter((type) => type.genus.id.toString() === genusIdStr)
                     .map((type) => (
-                        <li key={type.id}>
-                            <Link to={join(url, type.id.toString())}>
+                        <li className="type-list__item type-item" key={type.id}>
+                            <Link className="type-list__link type-link" to={join(url, type.id.toString())}>
                                 {type.name}
                             </Link>
                         </li>
                     ))}
             </ul>
-        </nav>
+        </div>
     );
 }
 
