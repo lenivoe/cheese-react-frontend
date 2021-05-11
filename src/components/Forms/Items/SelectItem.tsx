de32f4849a5229f8f7b56057d8a18db6a91d4b2e
lenivoe <levishok@gmail.com>
2021-05-12 04:37:21 +0700

    fixed default value for SelectItem

--- src/components/Forms/Items/SelectItem.tsx
+++ src/components/Forms/Items/SelectItem.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useField } from 'formik';
 import FormItemProps from './FormItemProps';
 
-export default function SelectItem({
+function SelectItem({
     placeholder = '<не задано>',
     label,
     wrapClass,
@@ -12,15 +12,20 @@ export default function SelectItem({
     ...props
 }: { placeholder?: string } & FormItemProps) {
     const [cssId] = useState(nanoid());
-    const [field] = useField(props);
+    const [{ value = SelectItem.UNSELECTED_VALUE, ...field }] = useField(props);
 
     return (
         <div className={wrapClass + ' form__field'}>
             <label htmlFor={cssId} className={labelClass + ' form-label'}>
                 {label}
             </label>
-            <select id={cssId} className={inputClass + ' form-input'} {...field}>
-                <option hidden disabled value=''>
+            <select
+                {...field}
+                id={cssId}
+                className={inputClass + ' form-input'}
+                value={value}
+            >
+                <option hidden disabled value={SelectItem.UNSELECTED_VALUE}>
                     {placeholder}
                 </option>
                 {props.children}
@@ -28,3 +33,7 @@ export default function SelectItem({
         </div>
     );
 }
+
+SelectItem.UNSELECTED_VALUE = '__UNSELECTED__VALUE__';
+
+export default SelectItem;
