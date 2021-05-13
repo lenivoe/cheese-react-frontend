87dd07b6e056a46485b58fa7283bdc490ac2aac1
lenivoe <levishok@gmail.com>
2021-05-13 08:19:42 +0700

    saving form is blocked without fetched data

--- src/components/Forms/StrainSavingForm/Fields.tsx
+++ src/components/Forms/StrainSavingForm/Fields.tsx
@@ -6,10 +6,16 @@ import TextItem from '../Items/TextItem';
 import withError from '../Items/hoc/WithError';
 import withStyle from '../Items/hoc/WithStyle';
 
+export const TextField = withError(withStyle(TextItem, 'strain-form'), 'form-error');
+
 const FormattedDate: typeof DateItem = (props) => (
     <DateItem {...props} format={DATE_FORMAT} />
 );
 
 export const DateField = withError(withStyle(FormattedDate, 'strain-form'), 'form-error');
-export const TextField = withError(withStyle(TextItem, 'strain-form'), 'form-error');
-export const SelectField = withError(withStyle(SelectItem, 'strain-form'), 'form-error');
+
+export const SelectField = withError(
+    withStyle(SelectItem, 'strain-form'),
+    'form-error'
+) as typeof SelectItem;
+SelectField.UNSELECTED_VALUE = SelectItem.UNSELECTED_VALUE;
--- src/components/Forms/StrainSavingForm/StrainSavingForm.tsx
+++ src/components/Forms/StrainSavingForm/StrainSavingForm.tsx
@@ -7,8 +7,9 @@ import moment from 'moment';
 import { getAllGenera, getAllStrainTypes, getStrain } from '../../../utils/data_fetch';
 import { DATE_FORMAT } from '../../../models/strain/strain';
 import { DateField, SelectField, TextField } from './Fields';
-import SelectItem from '../Items/SelectItem';
 import PropertiesList from './PropertiesList';
+import { delay } from '../../../utils/utils';
+import FormValues from './FormValues';
 
 interface UrlParams {
     strainId?: string;
@@ -19,141 +20,155 @@ export default function StrainSavingForm() {
 
     const fetchData = useCallback(async () => {
         const strainId = strainIdStr ? Number.parseInt(strainIdStr, 10) : null;
-        return strainId
-            ? Promise.all([getAllGenera(), getAllStrainTypes(), getStrain(strainId)])
-            : Promise.all([getAllGenera(), getAllStrainTypes()]);
+        return Promise.all([
+            getAllGenera(),
+            getAllStrainTypes(),
+            strainId ? getStrain(strainId) : undefined,
+            delay(5000),
+        ]);
     }, [strainIdStr]);
 
     const { data, error, isPending } = useAsync(fetchData);
+    const disabled = isPending || !!error;
 
-    if (isPending) {
-        return <p>Загрузка данных...</p>;
-    }
-    if (error) {
-        return <p>`Ошибка при получении данных: ${error.message}`</p>;
-    }
-
-    const [genusList, typeList, strain] = data ?? [[], []];
-
-    const bioProperties = strain?.properties.filter((property) => !property.isNote) ?? [];
-    const noteProperies = strain?.properties.filter((property) => property.isNote) ?? [];
+    const [genusList, typeList, strain] = data ?? [[], [], undefined, null];
 
     return (
-        <Formik
-            initialValues={{
-                genus: strain?.type?.genus,
-                type: strain?.type,
-                name: strain?.name ?? '',
-                dateReceiving: moment(strain?.dateReceiving).format(DATE_FORMAT),
-                collectionIndex: strain?.collectionIndex ?? '',
-                source: strain?.source ?? '',
-                obtainingMethod: strain?.obtainingMethod ?? '',
-                properties: { bio: bioProperties, note: noteProperies },
-            }}
-            validationSchema={() => {
-                const requiredMsg = 'обязательно';
+        <div className='strain-adding'>
+            {isPending ? <p>Загрузка данных...</p> : null}
+            {error ? <p>`Ошибка при получении данных: ${error.message}`</p> : null}
 
-                return Yup.object({
-                    genus: Yup.object().shape({
-                        name: Yup.string()
-                            .notOneOf([SelectItem.UNSELECTED_VALUE], requiredMsg)
-                            .required(requiredMsg),
-                    }),
-                    type: Yup.object().shape({
-                        name: Yup.string()
-                            .notOneOf([SelectItem.UNSELECTED_VALUE], requiredMsg)
-                            .required(requiredMsg),
-                    }),
-                    name: Yup.string().required(requiredMsg),
-                    dateReceiving: Yup.date().required(requiredMsg),
-                    collectionIndex: Yup.string().required(requiredMsg),
-                    source: Yup.string().required(requiredMsg),
-                    obtainingMethod: Yup.string().required(requiredMsg),
-                });
-            }}
-            onSubmit={(values, _helpers) => {
-                console.log(values);
-                alert(JSON.stringify(values));
-            }}
-        >
-            {({ values, setFieldValue }) => {
-                // В случае, когда Род и Вид штамма созданы только что (не содержатся в базе), у них нет id.
-                // Тогда, если выбран Род без id, то надо вывести Вид без id
-                const curTypeList = typeList.filter(
-                    ({ genus }) =>
-                        (!genus.id && !values.genus?.id) || genus.id === values.genus?.id
-                );
+            <Formik<FormValues>
+                enableReinitialize={true}
+                initialValues={{
+                    genus: strain?.type?.genus,
+                    type: strain?.type,
+                    name: strain?.name ?? '',
+                    dateReceiving: moment(strain?.dateReceiving).format(DATE_FORMAT),
+                    collectionIndex: strain?.collectionIndex ?? '',
+                    source: strain?.source ?? '',
+                    obtainingMethod: strain?.obtainingMethod ?? '',
+                    properties: {
+                        bio: strain?.properties.filter((prop) => !prop.isNote) ?? [],
+                        note: strain?.properties.filter((prop) => prop.isNote) ?? [],
+                    },
+                }}
+                validationSchema={() => {
+                    const requiredMsg = 'обязательно';
 
-                return (
-                    <div className='strain-adding'>
+                    return Yup.object({
+                        genus: Yup.object().shape({
+                            name: Yup.string()
+                                .notOneOf([SelectField.UNSELECTED_VALUE], requiredMsg)
+                                .required(requiredMsg),
+                        }),
+                        type: Yup.object().shape({
+                            name: Yup.string()
+                                .notOneOf([SelectField.UNSELECTED_VALUE], requiredMsg)
+                                .required(requiredMsg),
+                        }),
+                        name: Yup.string().required(requiredMsg),
+                        dateReceiving: Yup.date().required(requiredMsg),
+                        collectionIndex: Yup.string().required(requiredMsg),
+                        source: Yup.string().required(requiredMsg),
+                        obtainingMethod: Yup.string().required(requiredMsg),
+                    });
+                }}
+                onSubmit={(values, _helpers) => {
+                    console.log(values);
+                    alert(JSON.stringify(values));
+                }}
+            >
+                {({ values, setFieldValue }) => {
+                    // В случае, когда Род и Вид штамма созданы только что (не содержатся в базе), у них нет id.
+                    // Тогда, если выбран Род без id, то надо вывести Вид без id
+                    const curTypeList = typeList.filter(
+                        ({ genus }) =>
+                            (!genus.id && !values.genus?.id) ||
+                            genus.id === values.genus?.id
+                    );
+
+                    return (
                         <Form className='strain-form form--position-block-center'>
-                            <SelectField
-                                name='genus.name'
-                                label='Род'
-                                onValueChange={(_name, value) => {
-                                    const genus = genusList.find(
-                                        (genus) => genus.name === value
-                                    );
-                                    setFieldValue('genus', genus);
-                                    setFieldValue('type', undefined);
-                                }}
-                            >
-                                {genusList.map((genus) => (
-                                    <option key={genus.id} value={genus.name}>
-                                        {genus.name}
-                                    </option>
-                                ))}
-                            </SelectField>
-
-                            <SelectField
-                                name='type.name'
-                                label='Вид'
-                                onValueChange={(_name, value) => {
-                                    const type = curTypeList.find(
-                                        (type) => type.name === value
-                                    );
-                                    setFieldValue('type', type);
-                                }}
-                            >
-                                {curTypeList.map((type) => (
-                                    <option key={type.id} value={type.name}>
-                                        {type.name}
-                                    </option>
-                                ))}
-                            </SelectField>
-
-                            {/* часть наименования */}
-                            <TextField label='Исхродный индекс' name='name' />
-                            {/* часть наименования */}
-                            <DateField label='Дата получения' name='dateReceiving' />
-                            {/* каталожный индекс */}
-                            <TextField label='Индекс штаммов' name='collectionIndex' />
-                            {/* происхождение */}
-                            <TextField label='Происхождение' name='source' />
-                            {/* способ получения */}
-                            <TextField label='Способ получения' name='obtainingMethod' />
-
-                            <PropertiesList propType='bio' />
-                            <PropertiesList propType='note' />
-
-                            <div className='strain-adding__buttons form-buttons'>
-                                <button
-                                    type='submit'
-                                    className='form-buttons__submit-button form-button submit-button'
+                            <fieldset disabled={disabled}>
+                                <SelectField
+                                    name='genus.name'
+                                    label='Род'
+                                    onValueChange={(_name, value) => {
+                                        const genus = genusList.find(
+                                            (genus) => genus.name === value
+                                        );
+                                        setFieldValue('genus', genus);
+                                        setFieldValue('type', undefined);
+                                    }}
                                 >
-                                    Добавить
-                                </button>
-                                <button
-                                    type='button'
-                                    className='form-buttons__cancel-button form-button cancel-button'
+                                    {genusList.map((genus) => (
+                                        <option key={genus.id} value={genus.name}>
+                                            {genus.name}
+                                        </option>
+                                    ))}
+                                </SelectField>
+
+                                <SelectField
+                                    name='type.name'
+                                    label='Вид'
+                                    onValueChange={(_name, value) => {
+                                        const type = curTypeList.find(
+                                            (type) => type.name === value
+                                        );
+                                        setFieldValue('type', type);
+                                    }}
                                 >
-                                    Отмена
-                                </button>
-                            </div>
+                                    {curTypeList.map((type) => (
+                                        <option key={type.id} value={type.name}>
+                                            {type.name}
+                                        </option>
+                                    ))}
+                                </SelectField>
+
+                                {/* часть наименования */}
+                                <TextField label='Исхродный индекс' name='name' />
+                                {/* часть наименования */}
+                                <DateField
+                                    label='Дата получения'
+                                    name='dateReceiving'
+                                    disabled={disabled}
+                                />
+                                {/* каталожный индекс */}
+                                <TextField
+                                    label='Индекс штаммов'
+                                    name='collectionIndex'
+                                />
+                                {/* происхождение */}
+                                <TextField label='Происхождение' name='source' />
+                                {/* способ получения */}
+                                <TextField
+                                    label='Способ получения'
+                                    name='obtainingMethod'
+                                />
+
+                                <PropertiesList propType='bio' />
+                                <PropertiesList propType='note' />
+
+                                <div className='strain-adding__buttons form-buttons'>
+                                    <button
+                                        type='submit'
+                                        className='form-buttons__submit-button form-button submit-button'
+                                    >
+                                        Добавить
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='form-buttons__cancel-button form-button cancel-button'
+                                    >
+                                        Отмена
+                                    </button>
+                                </div>
+                            </fieldset>
                         </Form>
-                    </div>
-                );
-            }}
-        </Formik>
+                    );
+                }}
+            </Formik>
+        </div>
     );
 }
